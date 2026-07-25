e939337783855c232cc12bff8acfee02ef190753
MarlonValles00k <marlonvalles2005@gmail.com>
2026-07-24 23:02:34 -0400

    Update func_divisa.py
    actualización de las tazas usd y euro (se desactualizaran en una hora lo más probable)
    
    nueva funcion taza personalizada:
    Puedes hacer tus conversiones con tus propias tazas (no se guardan cuando sales de la funcion)

--- src/main/python/func_divisa.py
+++ src/main/python/func_divisa.py
@@ -1,20 +1,100 @@
-"""
-Módulo para la conversión de divisas mediante un menú interactivo.
---- 
-Args:
-    None.
---- 
-Returns:
-    None.
---- 
-Raises:
-    Exception: Si ocurre un error inesperado.
---- 
-"""
-
 from func_comprobacion import validar_mayor_cero
-from utils import clear_window, module_error
+import utils
 
+def validarEntradaInt(limite,mensaje):
+    """
+    Args:
+        limite:Establece el limite de numero maximo
+        
+        mensaje: La indicacion de que debe ingresar el usuario
+    ---
+    Returns:
+        La entrada del usuario
+    ---
+    Raises:
+        except: puede detectar errores de entradas
+    ---
+    
+    """
+    while True:
+        try:
+            Entrada=int(input(f"{utils.ansi_text.CYAN}{mensaje}: {utils.ansi_text.RESET}"))
+            
+            if Entrada < 1 or Entrada > limite:
+                print(f"{utils.ansi_text.RED}Error, ese Numero esta fuera del limite{utils.ansi_text.RESET}")
+            
+            else:
+                return Entrada
+            
+        except ValueError:
+            print(f"{utils.ansi_text.RED}❌ Entrada inválida. Por favor, introduce un número entero.{utils.ansi_text.RESET}")
+            
+def validarEntradaFloat(mensaje):
+    """
+    Args:
+        mensaje: La indicacion de que debe ingresar el usuario
+    ---
+    Returns:
+        La entrada del usuario
+    ---
+    Raises:
+        except: puede detectar errores de entradas
+    ---
+    
+    """
+    while True:
+        try:
+            Entrada=float(input(f"{utils.ansi_text.BLUE}{mensaje}: {utils.ansi_text.RESET}"))
+            if Entrada <= 0:
+                print(f"{utils.ansi_text.RED}ERROR: Por favor ingrese un numero positivo mayor a 0")
+            else:
+                return Entrada
+        except ValueError:
+            print(f"{utils.ansi_text.RED}ERROR: Por favor ingrese un numero flotante")
+
+def taza_personalizada():
+    """
+    Puedes convertir dolares con tazas personalizadas
+    Args:
+        Nonw
+    ---
+    Returns:
+        None
+    ---
+    Raises:
+        except: puede detectar errores de entradas
+    ---
+    
+    """
+    taza_personalizada=None
+    while True:
+        
+        print("""Taza personalizada
+1.Ingresar taza personalizada(el valor de la taza no se guardara cuando salgas)
+2.Usar taza personalizada
+3.Salir""")
+        opcion=validarEntradaInt(limite=3,mensaje="Por favor ingrese una opcion")
+    
+        if opcion == 1:
+            taza_personalizada=validarEntradaFloat(mensaje="Por favor ingrese el valor de la taza")
+        
+        elif opcion == 2 and taza_personalizada==None:
+            taza_personalizada=validarEntradaFloat(mensaje="Por favor ingrese el valor de la taza") 
+            Valor=validarEntradaFloat(mensaje="Por favor ingrese el los dolares que quiere convertir")
+            print(f"\n{Valor} convertido a la taza personalizada es igual a: {Valor*taza_personalizada}")
+            input("\nPresione ENTER para continuar")
+               
+        elif opcion == 2 and not(taza_personalizada==None):
+            Valor=validarEntradaFloat(mensaje="Por favor ingrese el los dolares que quiere convertir")
+            print(f"\n{Valor} convertido a la taza personalizada es igual a: {Valor*taza_personalizada}")
+            input("\nPresione ENTER para continuar")
+        
+        elif opcion == 3:
+            print("\nSaliste de la funcion taza personalizada")
+            print("\nno se guardara el valor de la taza personalizada")
+            input("\nPresione ENTER para continuar")
+            break
+            
 def main():
     """
     Solicita al usuario las divisas de origen y destino, junto con el monto,
@@ -35,18 +115,20 @@ def main():
     # Bucle principal del programa
     while True:
         print("\n--- CONVERSOR DE DIVISAS ---")
-        print("1. Convertir")
-        print("2. Salir")
+        print("1. Convertir(Dolares, Euros, Bolivares)")
+        print("2. Convertir(Taza personalizada, Bolivares)")
+        print("3. Salir")
         
-        opcion = input("Elige una opción: ")
+        opcion = validarEntradaInt(limite=3,mensaje="Ingrese una opcion entre 1-3")
         
         # Si elige salir, terminamos la función
-        if opcion == "2":
+        if opcion == 3:
             print("Hasta luego.")
             return
-        # Si elige una opción inválida, reiniciamos el bucle
-        elif opcion != "1":
-            print("Opción inválida. Intenta de nuevo.")
+        
+        # funcion de taza personalizada
+        elif opcion == 2:
+            taza_personalizada()
             continue
 
         # Validación de la moneda que se desea convertir (origen)
@@ -73,21 +155,22 @@ def main():
         if de == "bolivares":
             base = monto
         elif de == "usd":
-            base = monto * 633.02
+            base = monto * 737.52
         elif de == "eur":
-            base = monto * 710.10
+            base = monto * 844,22
 
         # Paso 2: moneda a la que se desea convertir
         if a == "bolivares":
             resultado = base
         elif a == "usd":
-            resultado = base / 633.02
+            resultado = base / 737.52
         elif a == "eur":
-            resultado = base / 710.10
+            resultado = base / 844,22
 
         # Paso 3: conversión final de la moneda
         print(f"Resultado: {resultado:.2f}")
+        input("\nPresione ENTER para continuar")
 
 if __name__ == "__main__":
-    clear_window()
-    module_error(__name__, __file__, __package__, __doc__) 
+    utils.clear_window()
+    utils.module_error(__name__, __file__, __package__, __doc__) 
